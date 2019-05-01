8c264deb52c32c013b037f622c84b61bf5b04d86
Mario Medeiros <mariomedeiros@gmail.com>
2019-05-01 10:53:48 +0000

    changed IDs

--- datastore/callpoints.go
+++ datastore/callpoints.go
@@ -3,20 +3,23 @@ package datastore
 import "time"
 
 const (
+	// KindCallpoints represents the "table" name (Kind) to store Callpoints
 	KindCallpoints = "callpoints"
 )
 
-//Callpoint contains the structure to work with
+// Callpoint contains the structure to match the callpoints Datastore records
 type Callpoint struct {
-	CpID        string    `datastore:"cpId"`
+	// key
+	ID int64 // The integer ID used in the datastore.
+	// Stored data
+	CpID        string    `datastore:"cpID"`
 	Created     time.Time `datastore:"created"`
 	AbsAddress  string    `datastore:"absAddress"`
 	Label       string    `datastore:"label"`
 	Description string    `datastore:"description"`
 	Type        int32     `datastore:"type"`
 	Priority    int32     `datastore:"priority"`
-
+	// no indexing
 	Icon       string `datastore:"icon,noindex"`
 	RawRequest string `datastore:"rawRequest,noindex"`
-	ID         int64  // The integer ID used in the datastore.
 }
