e39890386e27f57d49208cc19745b041208f5de3
Mario Medeiros <mariomedeiros@gmail.com>
2019-05-01 15:25:46 +0000

    revised devices

---
+++ datastore/devices.go
@@ -0,0 +1,30 @@
+package datastore
+
+import "time"
+
+const (
+	// KindDevices represents the "table" name (Kind) to store Devices
+	KindDevices = "devices"
+)
+
+// Device contains the structure to match the devices Datastore records
+type Device struct {
+	// key
+	ID int64 // The integer ID used in the datastore.
+	// Stored data
+	DvID        string    `datastore:"dvID"`
+	Created     time.Time `datastore:"created"`
+	Label       string    `datastore:"label"`
+	Description string    `datastore:"description"`
+	Type        int32     `datastore:"type"`
+
+	Priority    int32  `datastore:"priority"`
+	IsTwoWay    bool   `datastore:"isTwoWay"`
+	Category    string `datastore:"category"`
+	Destination string `datastore:"destination"`
+
+	// no indexing
+	Settings   string `datastore:"settings,noindex"`
+	Icon       string `datastore:"icon,noindex"`
+	RawRequest string `datastore:"rawRequest,noindex"`
+}
