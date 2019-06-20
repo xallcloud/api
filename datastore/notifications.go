54d10bfe22aab69c4442792c9a09d045aec30fe1
mariomedeiros <mariomedeiros@gmail.com>
2019-06-20 23:57:23 +0000

    updates

--- datastore/notifications.go
+++ datastore/notifications.go
@@ -13,7 +13,7 @@ type Notification struct {
 	ID int64 `datastore:"-"` // The integer ID used in the datastore.
 	// Stored data
 	NtID          string    `datastore:"ntID"`
-	EvID          string    `datastore:"evID"`
+	AcID          string    `datastore:"acID"`
 	Priority      int32     `datastore:"priority"`
 	Category      string    `datastore:"category"`
 	Destination   string    `datastore:"destination"`
@@ -21,9 +21,6 @@ type Notification struct {
 	ResponseTitle string    `datastore:"responseTitle"`
 	Options       string    `datastore:"options"`
 	Created       time.Time `datastore:"created"`
-	EvSubType     string    `datastore:"evSubType"`
-	EvDescription string    `datastore:"evDescription"`
-	//??
-	//CallpointObj  Callpoint `datastore:"-"`
-	//DeviceObj     Device    `datastore:"-"`
+	CallpointObj  Callpoint `datastore:"-"`
+	DeviceObj     Device    `datastore:"-"`
 }
